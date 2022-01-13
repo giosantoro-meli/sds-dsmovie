bd6c6b7ed2946121ba45eee3909ad0bff527e23e
giosantoro-meli <giovanni.santoro@mercadolivre.com>
2022-01-13 19:14:10 -0300

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dsmovie.services;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class MovieService {
+
+    private final MovieRepository repository;
+
+    public MovieService(MovieRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<MovieDTO> findAll(Pageable pageable){
+        Page<Movie> result = repository.findAll(pageable);
+        return result.map(MovieDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public MovieDTO findById(Long id){
+        return new MovieDTO(repository.findById(id).get());
+    }
+}
