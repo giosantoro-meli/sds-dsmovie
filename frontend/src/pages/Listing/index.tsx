ccc57ba35e0969030a5eac6763aa0968d74aa339
giosantoro-meli <giovanni.santoro@mercadolivre.com>
2022-01-20 09:02:25 -0300

    useParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,6 +7,7 @@ import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [pageNumber, setPageNumber] = useState(0);
 
     const [page, setPage] = useState<MoviePage>(
